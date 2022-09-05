8a877dc05bf7b5fab221f56fabe156490c733365
Adrien <belhommeadrien@gmail.comn>
2022-09-05 21:11:48 +0200

    second commit to add the resst of the files I forgot

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import useStyles from './styles';
+import { SideBar } from '..';
 
 const NavBar = () => {
-  console.log('NavBar');
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
 
   return (
-    <div>
-      NavBar
-    </div>
+    <>
+      <AppBar position="fixed" className={classes.appbar}>
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => {}}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/:id"
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://images.assetsdelivery.com/compings_v2/nexusby/nexusby1810/nexusby181000286.jpg"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <SideBar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent" open>
+              <SideBar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+
+      </div>
+    </>
   );
 };
 
